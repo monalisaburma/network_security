1a762a591f28f2bb081f733bb547ae5e4b5b815d
Monalisa Burma <monalisaburma@gmail.com>
2024-08-24 13:30:02 +0530

    update the changes

--- networksecurity/entity/artifact_entity.py
+++ networksecurity/entity/artifact_entity.py
@@ -22,6 +22,11 @@ class DataTransformationArtifact:
     transformed_test_file_path: str
 
 @dataclass
+class ClassificationMetricArtifact:
+    f1_score: float
+    precision_score: float
+    recall_score: float
+@dataclass
 class ModelTrainerArtifact:
     trained_model_file_path: str
     train_metric_artifact: ClassificationMetricArtifact
@@ -33,8 +38,4 @@ class ModelEvaluationArtifact:
 
 @dataclass
 class ModelPusherArtifact:
-    pass
-
-@dataclass
-class ClassificationMetricArtifact:
     pass
